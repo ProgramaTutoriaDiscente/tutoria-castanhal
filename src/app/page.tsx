3ea0a4f9337fdcb0dbc96bb6ef6da128a133d4eb
Ali-Maia <aliciaengcomp@gmail.com>
2024-02-16 10:04:21 -0300

    fit: add HeroSection

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,10 @@
+import { HeroSection } from '@/components/@pages/home/hero-section/hero-section'
+
 export default function Home() {
   return (
     <>
       {/* TODO: Hero Section */}
-
+      <HeroSection />
       {/* TODO: Last News Section */}
 
       {/* TODO: Layout Section */}
