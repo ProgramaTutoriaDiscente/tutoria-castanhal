91bb53f032f406b4600baa0ebc5a4a2256f278fd
Ali-Maia <aliciaengcomp@gmail.com>
2024-02-16 18:27:18 -0300

    feat: add page-header

--- src/app/(pages)/blog/page.tsx
+++ src/app/(pages)/blog/page.tsx
@@ -1,10 +1,14 @@
+import { PageHeader } from '@/components/page-header/page-header'
 import React from 'react'
 
 export default function Blog() {
   return (
     <>
       {/* TODO: Page Header */}
-
+      <PageHeader
+        title="Blog"
+        description="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse varius enim in eros elementum tristique. "
+      />
       {/* TODO: Articles Section */}
     </>
   )
--- src/app/(pages)/sobre/page.tsx
+++ src/app/(pages)/sobre/page.tsx
@@ -1,9 +1,14 @@
+import { PageHeader } from '@/components/page-header/page-header'
 import React from 'react'
 
 export default function Sobre() {
   return (
     <>
       {/* TODO: Page Header */}
+      <PageHeader
+        title="Sobre o Projeto de Tutoria"
+        description="Nossa missão é ajudar estudantes a superar os desafios acadêmicos que enfrentam ao longo de sua jornada educacional e alcançar seu pleno potencial, desenvolvendo habilidades e competências que serão fundamentais para o seu sucesso futuro."
+      />
 
       {/* TODO: Areas Section */}
 
