7212b6900908e5fa501f9f2f68dc570fde86189b
Ali-Maia <aliciaengcomp@gmail.com>
2024-02-22 10:15:06 -0300

    feat: change pageheader style

--- src/components/shared/page-header.tsx
+++ src/components/shared/page-header.tsx
@@ -1,19 +1,23 @@
-import { cn } from '@/lib/utils'
-import { Section } from '../section/section'
+import { cn } from "@/lib/utils";
+import { Section } from "../section/section";
 
 interface PageHeaderProps {
-  title: string
-  description: string
-  className?: string
+  title: string;
+  description: string;
+  className?: string;
 }
 
 export function PageHeader({ title, description, className }: PageHeaderProps) {
   return (
-    <Section enableBg className={cn('border-b-2 border-black', className)}>
-      <div className="space-y-6 py-20 text-center">
-        <h2 className="text-5xl font-bold">{title}</h2>
-        <p className="text-lg">{description}</p>
+    <Section
+      enableBg
+      background={"gray"}
+      className={cn("border-b-2 border-secondary", className)}
+    >
+      <div className="mx-auto max-w-prose space-y-6 py-20 text-center">
+        <h1 className="text-5xl font-bold">{title}</h1>
+        <p className="text-lg text-muted-foreground ">{description}</p>
       </div>
     </Section>
-  )
+  );
 }
