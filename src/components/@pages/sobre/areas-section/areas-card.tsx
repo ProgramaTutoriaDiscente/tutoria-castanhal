96164c56d2a8fa5d8df639dd7b114257abd6e58d
Ali-Maia <aliciaengcomp@gmail.com>
2024-02-22 10:40:17 -0300

    feat: change areas-card style

--- src/components/@pages/sobre/areas-section/areas-card.tsx
+++ src/components/@pages/sobre/areas-section/areas-card.tsx
@@ -1,18 +1,18 @@
-import { cn } from '@/lib/utils'
-import { ChevronRight } from 'lucide-react'
-import Image from 'next/image'
-import Link from 'next/link'
+import { cn } from "@/lib/utils";
+import { ChevronRight } from "lucide-react";
+import Image from "next/image";
+import Link from "next/link";
 
 interface AreasCardProps {
-  title: string
-  description: string
-  background?: string
+  title: string;
+  description: string;
+  background?: string;
 }
 export function AreasCard({ title, description, background }: AreasCardProps) {
   return (
     <div
       className={cn(
-        'relative overflow-hidden rounded-2xl bg-cover bg-center',
+        "relative overflow-hidden rounded-2xl bg-cover bg-center",
         background,
       )}
     >
@@ -26,13 +26,16 @@ export function AreasCard({ title, description, background }: AreasCardProps) {
       <div className="flex h-[300px] flex-col justify-end bg-gradient-to-b   from-transparent to-foreground/85 lg:h-[500px] ">
         <div className="p-8 text-background">
           <span className="font-semibold">Apoio Acadêmico</span>
-          <h3 className="text-3xl font-bold">{title}</h3>
-          <p>{description}</p>
-          <Link href="#" className="flex gap-2">
+          <div className="space-y-2">
+            <h3 className="text-3xl font-bold">{title}</h3>
+            <p>{description}</p>
+          </div>
+
+          <Link href="#" className="flex gap-2 mt-6">
             Saiba Mais <ChevronRight />
           </Link>
         </div>
       </div>
     </div>
-  )
+  );
 }
