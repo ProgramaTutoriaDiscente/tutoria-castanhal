80decf9ca06419b752c93640ce9f3ecf4a94d05e
Aridan <aridanpantoja@gmail.com>
2024-02-21 22:55:21 -0300

    feat: add team item props

--- src/components/@pages/sobre/team-section/team-item.tsx
+++ src/components/@pages/sobre/team-section/team-item.tsx
@@ -3,33 +3,58 @@ import {
   AvatarFallback,
   AvatarImage,
 } from '@/components/shadcn-ui/avatar'
-import { Button } from '@/components/shadcn-ui/button'
+import { buttonVariants } from '@/components/shadcn-ui/button'
 import { CarouselItem } from '@/components/shadcn-ui/carousel'
+import Link from 'next/link'
 import { FaLinkedin } from 'react-icons/fa'
 import { LuGraduationCap } from 'react-icons/lu'
 
-export function TeamItem() {
+interface TeamItemProps {
+  name: string
+  avatarImage: string
+  avatarFallback: string
+  role: string
+  description: string
+  linkedin?: string
+  lattes?: string
+}
+
+export function TeamItem({
+  name,
+  avatarImage,
+  avatarFallback,
+  role,
+  description,
+  linkedin,
+  lattes,
+}: TeamItemProps) {
   return (
     <CarouselItem className="basis-1/2 md:basis-1/3">
       <div className="flex w-full flex-col overflow-hidden rounded-xl border bg-card p-6 text-card-foreground shadow">
         <Avatar className="h-16 w-16 border-2">
-          <AvatarImage src="https://github.com/aridanpantoja.png" />
-          <AvatarFallback>AP</AvatarFallback>
+          <AvatarImage src={avatarImage} />
+          <AvatarFallback>{avatarFallback}</AvatarFallback>
         </Avatar>
-        <h3 className="mt-1 text-xl font-bold">Aridan Pantoja</h3>
-        <span className="mt-1 text-muted-foreground">
-          Tutor de Informática Básica
-        </span>
-        <p className="mt-2 text-sm text-muted-foreground">
-          Engenharia de Computação (8º período)
-        </p>
+        <h3 className="mt-1 text-xl font-bold">{name}</h3>
+        <span className="mt-1 text-muted-foreground">{role}</span>
+        <p className="mt-2 text-sm text-muted-foreground">{description}</p>
         <div className="mt-4 space-x-2">
-          <Button size="icon" variant="outline">
-            <FaLinkedin className="h-5 w-5" />
-          </Button>
-          <Button size="icon" variant="outline">
-            <LuGraduationCap className="h-5 w-5" />
-          </Button>
+          {linkedin && (
+            <Link
+              href={linkedin}
+              className={buttonVariants({ variant: 'outline', size: 'icon' })}
+            >
+              <FaLinkedin className="h-5 w-5" />
+            </Link>
+          )}
+          {lattes && (
+            <Link
+              href={lattes}
+              className={buttonVariants({ variant: 'outline', size: 'icon' })}
+            >
+              <LuGraduationCap className="h-5 w-5" />
+            </Link>
+          )}
         </div>
       </div>
     </CarouselItem>
--- src/components/@pages/sobre/team-section/team-section.tsx
+++ src/components/@pages/sobre/team-section/team-section.tsx
@@ -1,27 +1,49 @@
 import { Section } from '@/components/section/section'
 import SectionHeader from '@/components/section/section-header'
 import {
-    Carousel,
-    CarouselContent,
-    CarouselNext,
-    CarouselPrevious,
+  Carousel,
+  CarouselContent,
+  CarouselNext,
+  CarouselPrevious,
 } from '@/components/shadcn-ui/carousel'
 import { TeamItem } from './team-item'
 
 export function TeamSection() {
   return (
-    <Section>
+    <Section background="gray">
       <SectionHeader
         title="Nossa equipe"
         description="Conheça mais sobre a equipe do Programa de Tutoria Discente Castanhal"
-        className="mb-8"
       />
-      <Carousel className="w-full px-2">
+      <Carousel className="mt-6 w-full px-2">
         <CarouselContent>
-          <TeamItem />
-          <TeamItem />
-          <TeamItem />
-          <TeamItem />
+          <TeamItem
+            name="Aridan Pantoja"
+            avatarImage="https://github.com/aridanpantoja.png"
+            avatarFallback="AP"
+            role="Tutor de Informática Básica"
+            description="Discente de Engenharia de Computação (8º período)"
+            linkedin="https://www.linkedin.com/in/aridan-pantoja/"
+            lattes="https://github.com/aridanpantoja"
+          />
+          <TeamItem
+            name="Alícia Maia"
+            avatarImage="https://github.com/Ali-Maia.png"
+            avatarFallback="AM"
+            role="Tutora de Informática Básica"
+            description="Discente de Engenharia de Computação (8º período)"
+            linkedin="https://www.linkedin.com/in/dev-maia/"
+            lattes="https://github.com/Ali-Maia"
+          />
+          <TeamItem
+            name="Aridan Pantoja"
+            avatarImage="https://github.com/aridanpantoja.png"
+            avatarFallback="AP"
+            role="Tutor de Informática Básica"
+            description="Discente de Engenharia de Computação (8º período)"
+            linkedin="https://www.linkedin.com/in/dev-maia/"
+            lattes="https://github.com/Ali-Maia"
+          />
         </CarouselContent>
         <CarouselPrevious />
         <CarouselNext />
