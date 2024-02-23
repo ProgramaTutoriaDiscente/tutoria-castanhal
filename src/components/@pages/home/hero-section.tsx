ca49f595c113c87a1526d833c220067d1cd24812
Aridan Pantoja <70522543+aridanpantoja@users.noreply.github.com>
2024-02-23 15:15:19 +0000

    refactor: remove className from Section

--- src/components/@pages/home/hero-section.tsx
+++ src/components/@pages/home/hero-section.tsx
@@ -5,7 +5,7 @@ import Link from 'next/link'
 
 export function HeroSection() {
   return (
-    <Section enableBg className="relative">
+    <Section enableBg>
       <div className="grid gap-8 lg:gap-6 ">
         <div className="flex flex-col items-center justify-center gap-4 ">
           <h1 className="text-center text-4xl font-bold tracking-tight sm:text-5xl">
