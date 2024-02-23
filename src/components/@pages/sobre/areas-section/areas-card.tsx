aa6a88cdb41b0a4e32bb93a445212563b09ffd29
Aridan Pantoja <70522543+aridanpantoja@users.noreply.github.com>
2024-02-23 13:09:53 +0000

    fix: change areas card and section

--- src/components/@pages/sobre/areas-section/areas-card.tsx
+++ src/components/@pages/sobre/areas-section/areas-card.tsx
@@ -1,4 +1,3 @@
-import { cn } from "@/lib/utils";
 import { ChevronRight } from "lucide-react";
 import Image from "next/image";
 import Link from "next/link";
@@ -6,32 +5,24 @@ import Link from "next/link";
 interface AreasCardProps {
   title: string;
   description: string;
-  background?: string;
+  background: string;
 }
 export function AreasCard({ title, description, background }: AreasCardProps) {
   return (
-    <div
-      className={cn(
-        "relative overflow-hidden rounded-2xl bg-cover bg-center",
-        background,
-      )}
-    >
-      {/* <Image 
-        src={background}
-        width={500}
-        height={500}
-        alt="imagem ilustrativa"
-        className="absolute -z-10 h-full"
-      /> */}
-      <div className="flex h-[300px] flex-col justify-end bg-gradient-to-b   from-transparent to-foreground/85 lg:h-[500px] ">
-        <div className="p-8 text-background">
-          <span className="font-semibold">Apoio Acadêmico</span>
-          <div className="space-y-2">
-            <h3 className="text-3xl font-bold">{title}</h3>
-            <p>{description}</p>
-          </div>
-
-          <Link href="#" className="flex gap-2 mt-6">
+    <div className="overflow-hidden rounded-2xl w-full border shadow">
+      <div className="relative h-[256px] sm:h-[364px]">
+        <div className="absolute top-0 right-0 bottom-0 left-0 bg-gradient-to-t from-foreground to-transparent" />
+        <Image 
+            src={background}
+            width={800}
+            height={800}
+            alt="imagem ilustrativa"
+            className="absolute top-0 right-0 bottom-0 left-0 -z-10 h-contain"
+        />
+        <div className="absolute bottom-0 p-6 text-background space-y-2">
+          <h3 className="text-2xl sm:text-3xl font-bold">{title}</h3>
+          <p className="text-sm">{description}</p>
+          <Link href="#" className="inline-flex text-sm sm:text-base font-semibold">
             Saiba Mais <ChevronRight />
           </Link>
         </div>
--- src/components/@pages/sobre/areas-section/areas-section.tsx
+++ src/components/@pages/sobre/areas-section/areas-section.tsx
@@ -1,34 +1,31 @@
+import { Section } from "@/components/section/section";
 import SectionHeader from "@/components/section/section-header";
 import { AreasCard } from "./areas-card";
-import { Section } from "@/components/section/section";
-// import CardImageLinguagens from 'public/images/areas-card-image-linguagens.jpg'
-// import CardImageMatematica from 'public/images/areas-card-image-matematica.jpg'
-// import CardImageInformatica from 'public/images/areas-card-image-informatica.jpg'
 
 export function AreasSection() {
   return (
       <Section>
-          <SectionHeader title="Áreas" subtitle="Apoio Academico" description="Tutoria online para estudantes com dificuldades acadêmicas" className=" items-center" />
-          <div className="h-max-[640px] grid h-full gap-6 lg:grid-cols-3">
+          <SectionHeader 
+              title="Áreas"
+              description="Tutoria online para estudantes com dificuldades acadêmicas"
+          />
+          <div className="grid grid-cols-1 gap-6 sm:grid-cols-2 lg:grid-cols-3 lg:gap-8">
             <AreasCard
               title="Linguagens"
-              description="Apoio acadêmico personalizado para ajudar você a ter sucesso."
-              background={"bg-[url('/images/areas-card-image-linguagens.jpg')]"}
+              description="Apoio acadêmico personalizado para ajudar você a ter sucesso"
+              background="/images/areas-card-image-linguagens.jpg"
             />
             <AreasCard
               title="Matemática"
-              description="Oferecer suporte acadêmico de qualidade para estudantes."
-              background={"bg-[url('/images/areas-card-image-matematica.jpg')]"}
+              description="Oferecer suporte acadêmico de qualidade para estudantes"
+              background="/images/areas-card-image-matematica.jpg"
             />
             <AreasCard
               title="Informática"
-              description="Contribuir para o sucesso acadêmico de estudantes em todo o país."
-              background={
-                "bg-[url('/images/areas-card-image-informatica.jpg')]"
-              }
+              description="Contribuir com o sucesso acadêmico de estudantes do país"
+              background="/images/areas-card-image-informatica.jpg"
             />
           </div>
       </Section>
-  );
+  )
 }
-// bg-[url('/images/areas-card-image-linguagens.jpg')]
