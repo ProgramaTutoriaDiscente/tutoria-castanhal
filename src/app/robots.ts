34db1895ffc87ae5627defa9a97c1ba0ec5c71e2
Aridan Pantoja <70522543+aridanpantoja@users.noreply.github.com>
2024-02-23 14:34:49 +0000

    feat: add robots.txt

--- src/app/robots.ts
+++ src/app/robots.ts
@@ -1,1 +1,11 @@
-// TODO: Robots.ts
+import { MetadataRoute } from 'next'
+
+export default function robots(): MetadataRoute.Robots {
+  return {
+    rules: {
+      userAgent: '*',
+      allow: '/',
+    },
+    sitemap: 'https://tutoriacastanhal.com.br/sitemap.xml',
+  }
+}
