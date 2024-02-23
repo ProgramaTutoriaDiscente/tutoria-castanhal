779a2481db75fdf37522fea50eab27b1d5595224
Ali-Maia <aliciaengcomp@gmail.com>
2024-02-23 11:00:41 -0300

    feat: mod prettier style

--- src/components/@pages/sobre/areas-section/areas-card.tsx
+++ src/components/@pages/sobre/areas-section/areas-card.tsx
@@ -1,32 +1,35 @@
-import { ChevronRight } from "lucide-react";
-import Image from "next/image";
-import Link from "next/link";
+import { ChevronRight } from 'lucide-react'
+import Image from 'next/image'
+import Link from 'next/link'
 
 interface AreasCardProps {
-  title: string;
-  description: string;
-  background: string;
+  title: string
+  description: string
+  background: string
 }
 export function AreasCard({ title, description, background }: AreasCardProps) {
   return (
-    <div className="overflow-hidden rounded-2xl w-full border shadow">
+    <div className="w-full overflow-hidden rounded-2xl border shadow">
       <div className="relative h-[256px] sm:h-[364px]">
-        <div className="absolute top-0 right-0 bottom-0 left-0 bg-gradient-to-t from-foreground to-transparent" />
-        <Image 
-            src={background}
-            width={800}
-            height={800}
-            alt="imagem ilustrativa"
-            className="absolute top-0 right-0 bottom-0 left-0 -z-10 h-contain"
+        <div className="absolute bottom-0 left-0 right-0 top-0 bg-gradient-to-t from-foreground to-transparent" />
+        <Image
+          src={background}
+          width={800}
+          height={800}
+          alt="imagem ilustrativa"
+          className="h-contain absolute bottom-0 left-0 right-0 top-0 -z-10"
         />
-        <div className="absolute bottom-0 p-6 text-background space-y-2">
-          <h3 className="text-2xl sm:text-3xl font-bold">{title}</h3>
+        <div className="absolute bottom-0 space-y-2 p-6 text-background">
+          <h3 className="text-2xl font-bold sm:text-3xl">{title}</h3>
           <p className="text-sm">{description}</p>
-          <Link href="#" className="inline-flex text-sm sm:text-base font-semibold">
+          <Link
+            href="#"
+            className="inline-flex text-sm font-semibold sm:text-base"
+          >
             Saiba Mais <ChevronRight />
           </Link>
         </div>
       </div>
     </div>
-  );
+  )
 }
--- src/components/@pages/sobre/areas-section/areas-section.tsx
+++ src/components/@pages/sobre/areas-section/areas-section.tsx
@@ -1,31 +1,31 @@
-import { Section } from "@/components/section/section";
-import SectionHeader from "@/components/section/section-header";
-import { AreasCard } from "./areas-card";
+import { Section } from '@/components/section/section'
+import SectionHeader from '@/components/section/section-header'
+import { AreasCard } from './areas-card'
 
 export function AreasSection() {
   return (
-      <Section>
-          <SectionHeader 
-              title="Áreas"
-              description="Tutoria online para estudantes com dificuldades acadêmicas"
-          />
-          <div className="grid grid-cols-1 gap-6 sm:grid-cols-2 lg:grid-cols-3 lg:gap-8">
-            <AreasCard
-              title="Linguagens"
-              description="Apoio acadêmico personalizado para ajudar você a ter sucesso"
-              background="/images/areas-card-image-linguagens.jpg"
-            />
-            <AreasCard
-              title="Matemática"
-              description="Oferecer suporte acadêmico de qualidade para estudantes"
-              background="/images/areas-card-image-matematica.jpg"
-            />
-            <AreasCard
-              title="Informática"
-              description="Contribuir com o sucesso acadêmico de estudantes do país"
-              background="/images/areas-card-image-informatica.jpg"
-            />
-          </div>
-      </Section>
+    <Section>
+      <SectionHeader
+        title="Áreas"
+        description="Tutoria online para estudantes com dificuldades acadêmicas"
+      />
+      <div className="grid grid-cols-1 gap-6 sm:grid-cols-2 lg:grid-cols-3 lg:gap-8">
+        <AreasCard
+          title="Linguagens"
+          description="Apoio acadêmico personalizado para ajudar você a ter sucesso"
+          background="/images/areas-card-image-linguagens.jpg"
+        />
+        <AreasCard
+          title="Matemática"
+          description="Oferecer suporte acadêmico de qualidade para estudantes"
+          background="/images/areas-card-image-matematica.jpg"
+        />
+        <AreasCard
+          title="Informática"
+          description="Contribuir com o sucesso acadêmico de estudantes do país"
+          background="/images/areas-card-image-informatica.jpg"
+        />
+      </div>
+    </Section>
   )
 }
