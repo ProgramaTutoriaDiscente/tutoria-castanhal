47e023549aac2fb4b8602168d306fed259a94d27
Ali-Maia <aliciaengcomp@gmail.com>
2024-03-04 17:40:12 -0300

    feat: add not-found page

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -8,21 +8,30 @@ import React from 'react'
 export default function NotFound() {
   // TODO: 404 Page
   return (
-  <Section className='relative h-full w-full'>
-    <div className="absolute -z-10 bottom-0 left-0 right-0 top-0 bg-[linear-gradient(to_right,#4f4f4f2e_1px,transparent_1px),linear-gradient(to_bottom,#4f4f4f2e_1px,transparent_1px)] bg-[size:14px_24px] [mask-image:radial-gradient(ellipse_60%_50%_at_50%_0%,#000_70%,transparent_100%)]"/>
-    <div className='flex justify-between items-center'>
-      <div>
-        <div className='flex gap-2 items-center'>
-          <h1 className='text-center text-5xl'> <span className='font-bold'>404</span> Not Found</h1>
-          <Search className='color-foreground h-max'/>
+    <Section className="relative h-full w-full">
+      <div className="absolute bottom-0 left-0 right-0 top-0 -z-10 bg-[linear-gradient(to_right,#4f4f4f2e_1px,transparent_1px),linear-gradient(to_bottom,#4f4f4f2e_1px,transparent_1px)] bg-[size:14px_24px] [mask-image:radial-gradient(ellipse_60%_50%_at_50%_0%,#000_70%,transparent_100%)]" />
+      <div className="flex flex-col-reverse items-center md:flex-row md:justify-between">
+        <div className="">
+          <div className="flex items-center gap-2">
+            <h1 className="text-center text-5xl">
+              <span className="font-bold">404</span> Not Found
+            </h1>
+            <Search className="color-foreground h-max" />
+          </div>
+          <p className=" mb-4">
+            Oops! Ocorreu um erro e não conseguimos encontrar a página desejada.
+          </p>
+          <Link href={'/'} className={buttonVariants()}>
+            Voltar para home
+          </Link>
         </div>
-        <p>Oops! Ocorreu um erro e não conseguimos encontrar a página desejada.</p>
-        <Link href={'/'} className={buttonVariants()}>
-          Voltar para home
-        </Link>
+        <Image
+          src="/images/not-found-image.svg"
+          width={400}
+          height={400}
+          alt="imagem ilustrativa"
+        />
       </div>
-      <Image src="/images/not-found-image.svg" width={400} height={400} alt='imagem ilustrativa'/>
-    </div>
-  </Section>
+    </Section>
   )
 }
