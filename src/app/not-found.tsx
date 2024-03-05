bb682be6d14b2b52b0ebc78e14cce4afb2eea3b3
Ali-Maia <aliciaengcomp@gmail.com>
2024-03-05 19:26:51 -0300

    feat: change not-found style resposive

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -1,6 +1,5 @@
 import { Section } from '@/components/section/section'
 import { buttonVariants } from '@/components/shadcn-ui/button'
-import { Search } from 'lucide-react'
 import Image from 'next/image'
 import Link from 'next/link'
 
@@ -8,21 +7,21 @@ export default function NotFound() {
   return (
     <Section className="relative h-full w-full">
       <div className="absolute bottom-0 left-0 right-0 top-0 -z-10 bg-[linear-gradient(to_right,#4f4f4f2e_1px,transparent_1px),linear-gradient(to_bottom,#4f4f4f2e_1px,transparent_1px)] bg-[size:14px_24px] [mask-image:radial-gradient(ellipse_60%_50%_at_50%_0%,#000_70%,transparent_100%)]" />
-      <div className="flex items-center justify-between">
-        <div>
-          <div className="flex items-center gap-2">
-            <h1 className="text-center text-5xl">
-              {' '}
-              <span className="font-bold">404</span> Not Found
+      <div className="flex flex-col-reverse items-center lg:flex-row lg:justify-between">
+        <div className="flex flex-col items-center lg:items-start">
+          <div className="mb-4 text-center lg:text-start">
+            <h1 className="text-4xl font-bold">
+              404<span className="text-primary">!</span> Não encontrado
             </h1>
-            <Search className="color-foreground h-max" />
+            <p className=" text-lg">
+              Oops! Ocorreu um erro e nos perdemos no caminho...
+            </p>
           </div>
-          <p>
-            Oops! Ocorreu um erro e não conseguimos encontrar a página desejada.
-          </p>
-          <Link href={'/'} className={buttonVariants()}>
-            Voltar para home
-          </Link>
+          <div>
+            <Link href={'/'} className={buttonVariants()}>
+              Voltar para home
+            </Link>
+          </div>
         </div>
         <Image
           src="/images/not-found-image.svg"
