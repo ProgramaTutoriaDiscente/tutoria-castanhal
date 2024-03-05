c5e19f060fc28096c1c69a80ce00a3759d1289d3
Ali-Maia <aliciaengcomp@gmail.com>
2024-03-05 20:26:43 -0300

    feat: add areas-card-modal

--- src/components/@pages/sobre/areas-section/areas-card/areas-card.tsx
+++ src/components/@pages/sobre/areas-section/areas-card/areas-card.tsx
@@ -1,13 +1,21 @@
-import { ChevronRight } from 'lucide-react'
 import Image, { StaticImageData } from 'next/image'
-import Link from 'next/link'
+
+import { AreasCardModal } from './areas-card-modal'
 
 interface AreasCardProps {
   title: string
   description: string
   background: StaticImageData
+  modalDescripition: string
+  modalDisciplinas: string
 }
-export function AreasCard({ title, description, background }: AreasCardProps) {
+export function AreasCard({
+  title,
+  description,
+  background,
+  modalDescripition,
+  modalDisciplinas,
+}: AreasCardProps) {
   return (
     <div className="w-full overflow-hidden rounded-2xl border shadow">
       <div className="relative h-[256px] sm:h-[364px]">
@@ -23,12 +31,11 @@ export function AreasCard({ title, description, background }: AreasCardProps) {
         <div className="absolute bottom-0 space-y-2 p-6 text-background">
           <h3 className="text-2xl font-bold sm:text-3xl">{title}</h3>
           <p className="text-sm">{description}</p>
-          <Link
-            href="#"
-            className="inline-flex text-sm font-semibold sm:text-base"
-          >
-            Saiba Mais <ChevronRight />
-          </Link>
+          <AreasCardModal
+            title={title}
+            description={description}
+            disciplinas={modalDisciplinas}
+          />
         </div>
       </div>
     </div>
